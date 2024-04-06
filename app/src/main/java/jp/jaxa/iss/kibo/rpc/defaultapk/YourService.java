26cb6e2165e3f0fabd6fc5db02b755ff9bcacc46
saimonssd-linux <simon.patino@udea.edu.co>
2024-04-06 17:56:29 -0500

    First integration

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -0,0 +1,28 @@
+package jp.jaxa.iss.kibo.rpc.defaultapk;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ */
+
+public class YourService extends KiboRpcService {
+    @Override
+    protected void runPlan1(){
+
+    }
+
+    @Override
+    protected void runPlan2(){
+        // write your plan 2 here
+
+
+    }
+
+    @Override
+    protected void runPlan3(){
+        // write your plan 3 here
+    }
+
+}
+
